f4886401fe7f7e775d88be2e9e63b043edd6e3a2
Ankush Makkar <ankushmakkar108@gmail.com>
2023-07-01 14:07:42 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <conio.h> 
+using namespace std;
+
+
+int main(){
+    int a = 10;
+    int b = 20;
+    int sum = a+b;
+    cout<<"The sum is : "<<sum<<endl;
+    
+    return 0;
+}

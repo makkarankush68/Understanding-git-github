8259eb1221a5207d3ea669b4785ef4c45b56f6ce
Ankush Makkar <ankushmakkar108@gmail.com>
2023-07-01 14:47:15 +0530

    division added

---
+++ division.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <conio.h> 
+using namespace std;
+
+
+int main(){
+    int a = 10;
+    int b = 5;
+    cout<<"division is : "<<a/b;
+    
+    return 0;
+}

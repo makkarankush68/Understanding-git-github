6a0937e311813cb84c850be7d066c34c8207e68f
Ankush Makkar <ankushmakkar108@gmail.com>
2023-07-01 14:29:19 +0530

    mulyiply done

---
+++ multiply.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <conio.h> 
+using namespace std;
+
+
+int main(){
+        int a = 10;
+    int b = 20;
+    int mul = a*b;
+    cout<<"The sum is : "<<mul<<endl;
+    
+    return 0;
+}
